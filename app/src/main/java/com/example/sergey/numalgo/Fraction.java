6698b4c712d10e7369d195a25c1ea35dc805c306
serzh272 <serzh272@mail.ru>
2018-11-18 17:16:07 +0900

    Add Fraction static functions

--- app/src/main/java/com/example/sergey/numalgo/Fraction.java
+++ app/src/main/java/com/example/sergey/numalgo/Fraction.java
@@ -1,6 +1,7 @@
 package com.example.sergey.numalgo;
 
 import android.print.PrinterId;
+import android.widget.Toast;
 
 public class Fraction {
     private long numerator;
@@ -63,10 +64,104 @@ public class Fraction {
         }
         else
         {
+            this.Normalize();
             return this.numerator + "/" + this.denominator;
         }
     }
     public void Read(String str){
+        if (str.indexOf('/') != -1)
+        {
+            String[] num_denom = str.split("/");
+            if (Long.valueOf(num_denom[1]) != 0)
+            {
+                this.numerator = Long.valueOf(num_denom[0]);
+                this.denominator = Long.valueOf(num_denom[1]);
+                this.Normalize();
+            }
+            else
+            {
 
+            }
+        }
+        else
+        {
+             this.numerator = Long.valueOf(str);
+             this.denominator = 1;
+        }
+    }
+    public static Fraction Add(Fraction fr1, Fraction fr2){
+        long nk = NOK(fr1.denominator, fr2.denominator);
+        long n1 = fr1.numerator * (nk / fr1.denominator);
+        long n2 = fr2.numerator * (nk / fr2.denominator);
+        return new Fraction(n1 + n2, nk);
+    }
+    public static Fraction Add(Fraction fr1, long n){
+        return new Fraction(fr1.numerator + n * fr1.denominator, fr1.denominator);
+    }
+    public static Fraction Add(long n, Fraction fr1){
+        return Add(fr1, n);
+    }
+    public static Fraction Sub(Fraction fr1, Fraction fr2){
+        long nk = NOK(fr1.denominator, fr2.denominator);
+        long n1 = fr1.numerator * (nk / fr1.denominator);
+        long n2 = fr2.numerator * (nk / fr2.denominator);
+        return new Fraction(n1 - n2, nk);
+    }
+    public static Fraction Sub(Fraction fr1, long n){
+        return new Fraction(fr1.numerator - n * fr1.denominator, fr1.denominator);
+    }
+    public static Fraction Sub(long n,Fraction fr1){
+        return new Fraction(n * fr1.denominator - fr1.numerator  , fr1.denominator);
+    }
+    public static Fraction Mult(Fraction fr1, Fraction fr2){
+        Fraction i = new Fraction(fr1.numerator, fr1.denominator);
+        Fraction j = new Fraction(fr2.numerator, fr2.denominator);
+        long l = j.numerator;
+        j.numerator = i.numerator;
+        i.numerator = l;
+        j.Normalize();
+        i.Normalize();
+        return new Fraction(j.numerator * i.numerator, j.denominator * i.denominator);
+    }
+    public static Fraction Mult(Fraction fr1, long n){
+        Fraction i = new Fraction(fr1.numerator, fr1.denominator);
+        Fraction j = new Fraction(n, 1);
+        return Mult(i, j);
+    }
+    public static Fraction Mult(long n, Fraction fr1){
+        return Mult(fr1, n);
+    }
+    public static Fraction Invert(Fraction fr1){
+        Fraction rez = new Fraction(fr1.numerator, fr1.denominator);
+        if (rez.numerator != 0)
+        {
+            long n = rez.denominator;
+            if (rez.numerator > 0)
+            {
+                rez.denominator = rez.numerator;
+                rez.numerator = n;
+            }
+            else
+            {
+                rez.denominator = -rez.numerator;
+                rez.numerator = -n;
+            }
+        }
+        return rez;
+    }
+    public static Fraction Div(Fraction fr1, Fraction fr2){
+        return Mult(fr1, Invert(fr2));
+    }
+    public static Fraction Div(Fraction fr1, long n){
+        Fraction fr = new Fraction(1, n);
+        return Mult(fr1, fr);
+    }
+    public static Fraction Div(long n,Fraction fr1){
+        Fraction fr = new Fraction(n, 1);
+        return Mult(fr, Invert(fr1));
+    }
+    public static boolean Equal(Fraction fr1,Fraction fr2){
+
+        return fr1.numerator == fr2.numerator && fr1.denominator == fr2.denominator;
     }
 }
--- app/src/main/java/com/example/sergey/numalgo/MainActivity.java
+++ app/src/main/java/com/example/sergey/numalgo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sergey.numalgo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void btn_click(View view) {
+        Fraction fr1 = new Fraction();
+        Fraction fr2 = new Fraction();
+        fr1.Read(((EditText)findViewById(R.id.editText)).getText().toString());
+        fr2.Read(((EditText)findViewById(R.id.editText2)).getText().toString());
+        if (view == findViewById(R.id.btn_add))
+        {
+            ((TextView)findViewById(R.id.textView)).setText(Fraction.Add(fr1, fr2).Write());
+        }
+        if (view == findViewById(R.id.btn_sub))
+        {
+            ((TextView)findViewById(R.id.textView)).setText(Fraction.Sub(fr1, fr2).Write());
+        }
+        if (view == findViewById(R.id.btn_mult))
+        {
+            ((TextView)findViewById(R.id.textView)).setText(Fraction.Mult(fr1, fr2).Write());
+        }
+        if (view == findViewById(R.id.btn_div))
+        {
+            ((TextView)findViewById(R.id.textView)).setText(Fraction.Div(fr1, fr2).Write());
+        }
     }
 }
